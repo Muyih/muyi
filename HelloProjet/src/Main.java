71eca9a8e2dfe49c781e0a77cfbd5adf36433411
yanghao <1163453575@qq.com>
2018-12-03 19:28:06 +0800

    修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,10 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+		System.out.println("Hel1111World!");
+		System.out.println("Hel2222World!");
+		System.out.println("Hell111orld!");
+		System.out.println("HelloWorld!");
+		System.out.println("Hello2rld!");
     }
 }
