4e0c898fca4e00a694adc55671cf1bb484c1f58b
geying <460219904@qq.com>
2018-12-03 19:19:14 +0800

    修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!1");
+		System.out.println("HelloWorld!2");
+		System.out.println("HelloWorld!3");
+		System.out.println("HelloWorld!4");
+		System.out.println("HelloWorld!5");
+		System.out.println("HelloWorld!6");
+		System.out.println("HelloWorld!7");
+		System.out.println("HelloWorld!8");
     }
 }
